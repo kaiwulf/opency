4fcc4f3bd2234295f5dac05d62f258ef9c95018c
kaiwulf <eigenspaces@gmail.com>
2020-10-11 10:58:41 -0400

    working on implementing sqlite3 interface

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
 require 'vendor/autoload.php';
+
+use opency\SQLiteConnection;
+
+$pdo = (new SQLiteConnection())->connect();
+
+/* try {
+   $this->pdo = new \PDO("sqlite:" . Config::PATH_TO_SQLITE_FILE);
+} catch (\PDOException $e) {
+   // handle the exception here
+} */
+
+if ($pdo != null)
+    echo 'Connected to the SQLite database successfully!';
+else
+    echo 'Could not connect to the SQLite database!';
